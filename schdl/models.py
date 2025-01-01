f1ebdd6d49ae907e70e667b8071d64192f06a07e
Anshuman Rath <anshumanrath030@gmail.com>
2025-01-01 18:11:40 +0530

    .

--- schdl/models.py
+++ schdl/models.py
@@ -1,67 +1,44 @@
-from django.http import JsonResponse
-from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.decorators import api_view, parser_classes
-from rest_framework import status
-from cloudinary.uploader import destroy
-from .models import BHP, LHP
-from .serializers import BHPSerializer, LHPSerializer
+from django.db import models
+from cloudinary.models import CloudinaryField
+import cloudinary
+
+class BHP(models.Model):
+    img = CloudinaryField('image', folder='Aahar/BH/', null=True, blank=True)
+    public_id = models.CharField(max_length=255, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.pk and self.public_id and 'img' in self.__dict__:
+            cloudinary.uploader.destroy(self.public_id)
+        if self.img:
+            self.public_id = self.img.public_id  
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.public_id:
+            cloudinary.uploader.destroy(self.public_id)
+        super().delete(*args, **kwargs)
+
+    def __str__(self):
+        return f"BHP {self.id} - {self.updated_at}"
 
 
-@api_view(['GET', 'POST'])
-@parser_classes([MultiPartParser, FormParser])
-def bhp_view(request):
-    """
-    Handle both GET and POST requests for the single BHP object (id=1).
-    """
-    return handle_image_view(request, BHP, BHPSerializer, "BHP")
+class LHP(models.Model):
+    img = CloudinaryField('image', folder='Aahar/LH/', null=True, blank=True)
+    public_id = models.CharField(max_length=255, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def save(self, *args, **kwargs):
+        if self.pk and self.public_id and 'img' in self.__dict__:
+            cloudinary.uploader.destroy(self.public_id)
+        if self.img:
+            self.public_id = self.img.public_id  
+        super().save(*args, **kwargs)
 
-@api_view(['GET', 'POST'])
-@parser_classes([MultiPartParser, FormParser])
-def lhp_view(request):
-    """
-    Handle both GET and POST requests for the single LHP object (id=1).
-    """
-    return handle_image_view(request, LHP, LHPSerializer, "LHP")
+    def delete(self, *args, **kwargs):
+        if self.public_id:
+            cloudinary.uploader.destroy(self.public_id)
+        super().delete(*args, **kwargs)
 
-
-def handle_image_view(request, model, serializer_class, model_name):
-    """
-    Handle GET and POST requests for models with a single image object.
-    Automatically replaces images when a new one is uploaded.
-
-    Args:
-        request: The HTTP request.
-        model: The model class (e.g., BHP or LHP).
-        serializer_class: The serializer class (e.g., BHPSerializer or LHPSerializer).
-        model_name: Name of the model (used for error messages).
-
-    Returns:
-        JsonResponse: The response to the client.
-    """
-    try:
-        # Get or create the object with id=1
-        obj, created = model.objects.get_or_create(id=1)
-
-        if request.method == 'GET':
-            # Serialize and return the object data
-            serializer = serializer_class(obj)
-            return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
-
-        elif request.method == 'POST':
-            # If a new image is provided, delete the old one
-            if 'img' in request.data and obj.public_id:
-                destroy(obj.public_id)  # Delete the existing image from Cloudinary
-
-            # Update the object with new data
-            serializer = serializer_class(obj, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return JsonResponse(serializer.data, status=status.HTTP_200_OK)
-            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    except model.DoesNotExist:
-        return JsonResponse(
-            {'error': f'{model_name} object with id=1 does not exist'},
-            status=status.HTTP_404_NOT_FOUND
-        )
+    def __str__(self):
+        return f"LHP {self.id} - {self.updated_at}"
--- schdl/views.py
+++ schdl/views.py
@@ -2,8 +2,10 @@ from django.http import JsonResponse
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework import status
-from .models import BHP
-from .serializers import BHPSerializer
+from cloudinary.uploader import destroy
+from .models import BHP, LHP
+from .serializers import BHPSerializer, LHPSerializer
+
 
 @api_view(['GET', 'POST'])
 @parser_classes([MultiPartParser, FormParser])
@@ -11,33 +13,8 @@ def bhp_view(request):
     """
     Handle both GET and POST requests for the single BHP object (id=1).
     """
-    try:
-        bhp_object, created = BHP.objects.get_or_create(id=1)  
+    return handle_image_view(request, BHP, BHPSerializer, "BHP")
 
-        if request.method == 'GET':
-            # Return the BHP object data
-            serializer = BHPSerializer(bhp_object)
-            return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
-        
-        elif request.method == 'POST':
-    
-            serializer = BHPSerializer(bhp_object, data=request.data, partial=True)  
-            if serializer.is_valid():
-                serializer.save()
-                return JsonResponse(serializer.data, status=status.HTTP_200_OK)
-            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    except BHP.DoesNotExist:
-        return JsonResponse({'error': 'BHP object with id=1 does not exist'}, status=status.HTTP_404_NOT_FOUND)
-
-
-
-from django.http import JsonResponse
-from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.decorators import api_view, parser_classes
-from rest_framework import status
-from .models import LHP
-from .serializers import LHPSerializer
 
 @api_view(['GET', 'POST'])
 @parser_classes([MultiPartParser, FormParser])
@@ -45,19 +22,46 @@ def lhp_view(request):
     """
     Handle both GET and POST requests for the single LHP object (id=1).
     """
+    return handle_image_view(request, LHP, LHPSerializer, "LHP")
+
+
+def handle_image_view(request, model, serializer_class, model_name):
+    """
+    Handle GET and POST requests for models with a single image object.
+    Automatically replaces images when a new one is uploaded.
+
+    Args:
+        request: The HTTP request.
+        model: The model class (e.g., BHP or LHP).
+        serializer_class: The serializer class (e.g., BHPSerializer or LHPSerializer).
+        model_name: Name of the model (used for error messages).
+
+    Returns:
+        JsonResponse: The response to the client.
+    """
     try:
-        lhp_object, created = LHP.objects.get_or_create(id=1)  
+        # Get or create the object with id=1
+        obj, created = model.objects.get_or_create(id=1)
 
         if request.method == 'GET':
-            serializer = LHPSerializer(lhp_object)
+            # Serialize and return the object data
+            serializer = serializer_class(obj)
             return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
-        
+
         elif request.method == 'POST':
-            serializer = LHPSerializer(lhp_object, data=request.data, partial=True)  
+            # If a new image is provided, delete the old one
+            if 'img' in request.data and obj.public_id:
+                destroy(obj.public_id)  # Delete the existing image from Cloudinary
+
+            # Update the object with new data
+            serializer = serializer_class(obj, data=request.data, partial=True)
             if serializer.is_valid():
                 serializer.save()
                 return JsonResponse(serializer.data, status=status.HTTP_200_OK)
             return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    except LHP.DoesNotExist:
-        return JsonResponse({'error': 'LHP object with id=1 does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    except model.DoesNotExist:
+        return JsonResponse(
+            {'error': f'{model_name} object with id=1 does not exist'},
+            status=status.HTTP_404_NOT_FOUND
+        )
