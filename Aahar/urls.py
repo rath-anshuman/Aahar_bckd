951bd8e8885c260dc12e0e19bf78bdb0e5c4afef
Anshuman Rath <anshumanrath030@gmail.com>
2025-01-21 14:00:03 +0530

    admin interface updated

--- Aahar/urls.py
+++ Aahar/urls.py
@@ -4,10 +4,13 @@ from django.views.decorators.csrf import csrf_exempt
 from account.views import login,logout
 from Aahar import views
 
+from .views import fetch_visitors
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('schdl.urls')),
     path('',views.abc,name='abc'),
+    path('fetch-visitors/',fetch_visitors,name='fetchvisit'),
     path('login/',csrf_exempt(login)),
     path('logout/',csrf_exempt(logout)),
 ]
--- Aahar/views.py
+++ Aahar/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from account.models import Visitor
-def abc(request):
+
+
+# def abc(request):
+#     visitors = Visitor.objects.all().order_by('-day')
+#     return render(request, 'a.html', {'visitors': visitors})
+
+
+def fetch_visitors(request):
     visitors = Visitor.objects.all().order_by('-day')
-    return render(request, 'a.html', {'visitors': visitors})
+    return render(request, 'partials/visitors_table.html', {'visitors': visitors})
+
+
+def abc(request):
+    return render(request, 'a.html')
