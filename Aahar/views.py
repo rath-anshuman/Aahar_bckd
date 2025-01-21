2db100de8427984112fb05538324fd60b80f1035
Anshuman Rath <anshumanrath030@gmail.com>
2025-01-21 13:16:05 +0530

    visitors showed on backend server doamin

--- Aahar/views.py
+++ Aahar/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from account.models import Visitor
 def abc(request):
-    return render(request,'a.html')
+    visitors = Visitor.objects.all().order_by('-day')
+    return render(request, 'a.html', {'visitors': visitors})
