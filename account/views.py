5d61378ebbe7c91107d4860b6d59eca9da51821c
Anshuman Rath <anshumanrath030@gmail.com>
2025-01-29 14:25:00 +0530

    ..

--- account/views.py
+++ account/views.py
@@ -18,11 +18,11 @@ ALLOWED_DOMAIN = [
 @api_view(['POST'])
 def login(request):
 
-    origin = request.META.get('HTTP_ORIGIN', '')
-    referer = request.META.get('HTTP_REFERER', '')
+    # origin = request.META.get('HTTP_ORIGIN', '')
+    # referer = request.META.get('HTTP_REFERER', '')
 
-    if ALLOWED_DOMAIN not in origin and ALLOWED_DOMAIN not in referer:
-        return JsonResponse({"error": "Unauthorized domain"}, status=403)
+    # if ALLOWED_DOMAIN not in origin and ALLOWED_DOMAIN not in referer:
+    #     return JsonResponse({"error": "Unauthorized domain"}, status=403)
     
     username = request.data.get('username')
     pssd = request.data.get('password')
